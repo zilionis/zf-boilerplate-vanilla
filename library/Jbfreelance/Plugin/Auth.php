409e41ac942fa877f925f7627e5b7810a0f28b81
Jason Brown <jason.brown@jbfreelance.co.uk>
2012-06-24 20:48:02 +0100

    Revert "Revert "added acl plugin""
    This reverts commit 2c5c4b1fbecfa288b9ac4fadb87c7c8825fe6238.

---
+++ library/Jbfreelance/Plugin/Auth.php
@@ -0,0 +1,69 @@
+<?php
+
+class Jbfreelance_Plugin_Auth extends \Zend_Controller_Plugin_Abstract
+{
+    protected $_acl;
+    protected $_auth;
+    
+    public function __construct(Jbfreelance_Acl $acl)
+    {
+        $this->_acl = $acl;
+        
+        // Get instance of Auth
+        $this->_auth = \Zend_Auth::getInstance();
+    }
+    
+    public function isValidRequest(\Zend_Controller_Request_Abstract $request)
+    {
+        $dispatcher = \Zend_Controller_Front::getInstance()->getDispatcher();
+        
+        if($dispatcher->isDispatchable($request))
+        {
+            // Use reflection to check if the action exists
+            $className = $dispatcher->getControllerClass($request);
+            $fullClassName = $dispatcher->loadClass($className);
+            $action = $dispatcher->getActionMethod($request);
+            $class = new \Zend_Reflection_Class($fullClassName);
+            
+            return $class->hasMethod($action);
+        }
+    }
+    
+    public function routeShutdown(\Zend_Controller_Request_Abstract $request)
+    {        
+        // Only perform check against valid requests 
+        if($this->isValidRequest($request))
+        {
+            $module = $request->getModuleName();
+            $controller = $request->getControllerName();
+            $action = $request->getActionName();
+            $resource = $module.":".$controller;
+            $role = $this->_acl->getCurrentRole();
+            
+            // Check if a user is logged and doesnt have access
+            if($this->_auth->hasIdentity() && !$this->_acl->isAllowed($role, $resource, $action))
+            {
+                // Direct to unauthorised page
+                $request->setModuleName('site');
+                $request->setControllerName('auth');
+                $request->setActionName('forbidden');
+
+                // Redirect to login page
+                $this->getResponse()->setHttpResponseCode(401);
+            }else if(!$this->_auth->hasIdentity() && !$this->_acl->isAllowed($role, $resource, $action)){
+                // Not logged in trying to access a restricted page
+                
+                $fm = \Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+                $fm->addMessage(array('error' => 'You\'ve tried to access a restricted area'));
+                
+                $request->setModuleName('site');
+                $request->setControllerName('auth');
+                $request->setActionName('login');
+                
+                // Redirect to login page
+                $this->getResponse()->setHttpResponseCode(403);
+            }
+        }
+    }
+}
+?>
